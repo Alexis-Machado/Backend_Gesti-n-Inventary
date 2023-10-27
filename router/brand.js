3cf47c9a7a3546a329594676df2c048b53d90834
Alex Machado <jmachadoa12@gmail.com>
2023-10-27 12:47:50 -0500

    Despliegue Backend✅

---
+++ router/brand.js
@@ -0,0 +1,104 @@
+const {Router} = require('express');
+const Brand = require('../models/Brand');
+const router = Router();
+const {validatorBrandEquipments} = require('../helpers/validator-brand-equipments');
+
+
+router.post('/', async function(req,res){
+    try{
+        const validator =  validatorBrandEquipments(req);
+       if(validator.length>0){
+           return res.status(400).send(validator);
+       }
+        let brand = new Brand();
+
+        const existBrand = await Brand.findOne({name: req.body.name});
+        if(existBrand){
+            return res.status(400).send("Marca ya existente");
+        }
+
+        brand.name = req.body.name;
+        brand.state = req.body.state;
+        brand.creationDate = new Date();
+        brand.updateDate = new Date();
+
+        brand = await brand.save();
+        res.send(brand);
+    }catch(error){
+        res.status(500).send("Ha ocurrido un error");
+        console.log(error);
+    }
+
+});
+router.get('/', async function(req,res){
+    try{
+        const brand = await Brand.find();
+        res.send(brand);
+
+    }catch(error){
+        console.log(error);
+        res.status(500).send("Ha ocurrido un error");
+    }
+
+});
+router.put('/:idBrand', async function(req,res){
+    try{
+        const validator =  await validatorBrandEquipments(req);
+       if(validator.length>0){
+           return res.status(400).send(validator);
+       }
+        let brand = await Brand.findById(req.params.idBrand);
+       
+
+        if(!brand){
+            res.status(400).send("Marca no encontrada");
+        };
+
+        brand.name = req.body.name;
+        brand.state = req.body.state;
+        brand.updateDate = new Date();
+
+        brand = await brand.save();
+        res.send(brand);
+
+
+
+    }catch(error){
+        console.log(error);
+        res.status(500).send("Ha ocurrido un error");
+    }
+
+});
+router.get('/:brandId', async function(req, res){
+    try{
+        const brandi = await Brand.findById(req.params.brandId);
+
+        if(!brandi){
+            return res.status(404).send('Ha ocurrido un erro');
+        };
+        res.send(brandi);
+
+    }catch(error){
+        console.log(error)
+    }
+} )
+
+router.delete('/:brandId', async function(req, res) {
+    try {
+        const brand = await Brand.findById(req.params.brandId);
+
+        if (!brand) {
+            return res.status(404).send('Marca no encontrada');
+        }
+
+        const brandName = brand.name; 
+        await Brand.findByIdAndRemove(req.params.brandId);
+
+        res.send(`La Marca ${brandName} fue eliminada correctamente`);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Ha ocurrido un error al eliminar la marca');
+    }
+});
+
+module.exports= router;
